46b66018a3643561fe23c5abe14d515e70ee480e
Bob Godwin <bobgodwinx@gmail.com>
2015-04-08 12:47:02 +0200

    Rename method to registerForUserLocalNotificationForApplication

--- Beacon Finder/AppDelegate.swift
+++ Beacon Finder/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         //Registering for a local notification service.
 
-        self.registerForUserLocalNotification(application)
+        self.registerForUserLocalNotificationForApplication(application)
         return true
     }
 
@@ -43,7 +43,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
     
-    func registerForUserLocalNotification(application: UIApplication){
+    func registerForUserLocalNotificationForApplication(application: UIApplication){
     
         let notificationForegroudAction = UIMutableUserNotificationAction()
         notificationForegroudAction.activationMode = .Foreground
